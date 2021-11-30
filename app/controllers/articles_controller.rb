7691a27c252fee07ecb3a34fd53f1756b99140b1
chesh <chesh@chesh.info>
2021-11-30 16:38:44 +0000

    added edit article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,23 +3,27 @@
 class ArticlesController < ApplicationController
   before_action :authenticate_user!
   before_action :set_article, only: %i[show edit update destroy]
+  before_action :new_article, only: %i[new]
 
   def index
     @articles = Article.all
   end
 
-  def new
+  def new; end
 
-  end
+  def edit; end
 
   def show; end
 
-  def preview
-    # @preview = find_preview(params[:link])
-    @preview = HTTParty.post('http://api.linkpreview.net/?key=91b947d8ad96d94eb94a67b2fdf887e5&q=https://www.bbc.co.uk/sounds/play/p01w66tg')
-  end
-
   private
+
+  def new_article
+    if current_user.try(:admin?)
+      @article = Article.new
+    else
+      redirect_to action: 'index'
+    end
+  end
 
   def set_article
     if current_user.try(:admin?)
